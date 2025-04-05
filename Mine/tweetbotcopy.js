07bd1aec051839822a70504947f287a39c6e398e
poisonoff <31033194+poisonoff@users.noreply.github.com>
2025-04-06 01:36:01 +0200

    Update tweetbotcopy.js

--- Mine/tweetbotcopy.js
+++ Mine/tweetbotcopy.js
@@ -1,12 +1,9 @@
 /*
-https://subscription.tapbots.net/subscription/2/verify url script-response-body tweetbotcopy.js
-
-hostname=*.tapbots.net
+https://push.tapbots.com/ivory/verify_subscription url script-response-body ivory.js
+hostname=*.tapbots.com
 */
 
 let obj = JSON.parse($response.body);
-
-obj["expires_at"]= 3476097520,
-obj["can_trial"]= true,
-
-$done({body: JSON.stringify(obj)});
+obj["expires_at"] = 3476097520;  // 2084-01-01
+obj["can_trial"] = true;
+$done({ body: JSON.stringify(obj) });
