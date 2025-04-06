900841c2502b27ce0d41206f606c583c1e7ab455
poisonoff <31033194+poisonoff@users.noreply.github.com>
2025-04-06 02:58:52 +0200

    Update ivory.js

--- Mine/ivory.js
+++ Mine/ivory.js
@@ -6,7 +6,8 @@ hostname=*.tapbots.net
 
 let obj = JSON.parse($response.body);
 
-obj["expires_at"] = 3476097520,
-obj["can_trial"] = true,
+obj["expires_at"] = 3476097520;
+obj["can_trial"] = true;
 
 $done({body: JSON.stringify(obj)});
+
